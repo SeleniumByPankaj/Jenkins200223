470e35d8b92fd7f57902a9015d7735cbe935529b
Pankaj <pankajbharambe101@gmail.com>
2023-02-22 07:31:43 +0530

    adding job chaining code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,3 @@
-package demo;
-
 public class HelloWorld {
 
 	public static void main(String[] args) {
---
+++ JobChain_02.java
@@ -0,0 +1,9 @@
+public class JobChain_02 {
+
+	public static void main(String[] args) {
+
+		System.out.println("This is child job chaining code");
+
+	}
+
+}
